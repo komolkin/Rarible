c1b0b3d51f327fe3dc1607f7c7787c27ef8fc8cf
Ilya Komolkin <komolkin@gmail.com>
2022-12-25 03:59:19 +0300

    Fix

--- code.ts
+++ code.ts
@@ -62,3 +62,10 @@ figma.ui.onmessage = (pluginMessage) => {
   }
   figma.closePlugin();
 };
+
+// function randomIntFromInterval(min, max) { // min and max included
+//   return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+// const rndInt = randomIntFromInterval(1, 6)
+// console.log(rndInt)
